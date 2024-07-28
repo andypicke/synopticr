b6551d12e42ad231a94ec28485e250f2431e4a5f
Andy <andypicke@gmail.com>
2024-07-28 15:53:03 -0600

    update get_latest_data function - unpack observations

--- R/get_latest_data.R
+++ R/get_latest_data.R
@@ -1,4 +1,3 @@
-
 #' Get latest data from weather stations
 #'
 #' @param wh_state State to get data for
@@ -17,15 +16,15 @@ get_latest_data <- function(wh_state = "CO", token = Sys.getenv("SYNOPTIC_API_KE
   #
   req_url <- paste0(base_url,endpoint,"?token=", token, "&state=", wh_state)
   #req_url <- paste0(base_url,endpoint,"?token=", token)
-#  req_url <- paste0(base_url,endpoint,"?token=", token, "&stid=", station_id)
- # req_url
+  #req_url <- paste0(base_url,endpoint,"?token=", token, "&stid=", station_id)
   resp <- httr::GET(req_url)
 
 #  resp$status_code
 
-
   resp_parsed <- jsonlite::fromJSON(httr::content(resp, as = "text", encoding = "UTF-8"))
 
   df <- resp_parsed$STATION
-  #df <- resp_parsed$STATION$OBSERVATIONS
+
+  df2 <- tidyr::unnest(df,'OBSERVATIONS') # each station (row) contains a dataframe with observations
+
 }
