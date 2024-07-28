4566deadc9ac2b0c1bd7073079a1112250d9cac3
Andy <andypicke@gmail.com>
2024-07-28 15:20:37 -0600

    update function get_metadata

--- R/get_metadata.R
+++ R/get_metadata.R
@@ -1,4 +1,20 @@
 
-get_metadata <- function(){
-  print("hello")
+#' Get station metadata from synopticdata API
+#'
+#' @param wh_state State to get info for. Default "CO"
+#' @param token token (api key). Default is Sys.getenv("SYNOPTIC_API_KEY")
+#'
+#' @return df: Data frame of requested station metadata
+#' @export
+#'
+#' @examples meta <- get_metadata()
+get_metadata <- function(wh_state = "CO", token = Sys.getenv("SYNOPTIC_API_KEY")){
+
+  base_url <- "https://api.synopticdata.com/v2/"
+  endpoint <- "stations/metadata"
+  #req_url <- paste0(base_url,endpoint,"?token=", token)
+  req_url <- paste0(base_url,endpoint,"?token=", token, "&state=", wh_state)
+  resp <- httr::GET(req_url)
+  resp_parsed <- jsonlite::fromJSON(httr::content(resp, as = "text", encoding = "UTF-8"))
+  df <- resp_parsed$STATION # for data
 }
