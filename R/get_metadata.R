02fd8d36d6dd5b297a637dd9386ef2716e2f3134
Andy <andypicke@gmail.com>
2024-07-31 15:25:09 -0600

    convert lat/lon to numeric

--- R/get_metadata.R
+++ R/get_metadata.R
@@ -1,4 +1,3 @@
-
 #' Get station metadata from synopticdata API
 #'
 #' @param wh_state State to get info for. Default "CO"
@@ -17,4 +16,10 @@ get_metadata <- function(wh_state = "CO", token = Sys.getenv("SYNOPTIC_API_KEY")
   resp <- httr::GET(req_url)
   resp_parsed <- jsonlite::fromJSON(httr::content(resp, as = "text", encoding = "UTF-8"))
   df <- resp_parsed$STATION # for data
+
+  df$LATITUDE <- as.numeric(df$LATITUDE)
+  df$LONGITUDE <- as.numeric(df$LONGITUDE)
+
+  return(df)
+
 }
