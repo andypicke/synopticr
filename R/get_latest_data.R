bb7eedfefbe001b875d3532fd270e381a90efa2e
Andy <andypicke@gmail.com>
2024-08-01 10:30:34 -0600

    add units option to get_latest_data

--- R/get_latest_data.R
+++ R/get_latest_data.R
@@ -1,24 +1,30 @@
 #' Get latest data from weather stations
 #'
 #' @param wh_station Station to get data for.
-#' @param wh_state State to get data for. Default NULL. If specified will get data for all stations in state.
+#' @param wh_state State (abbreviated) to get data for. Default NULL. If specified will get data for all stations in state.
+#' @param units c("english", "metric") - Default "english"
 #' @param token token (api key). Default is Sys.getenv("SYNOPTIC_API_KEY")
 #'
 #' @return Dataframe of latest data for requested stations
 #' @export
-get_latest_data <- function(wh_station = "KDEN", wh_state = NULL, token = Sys.getenv("SYNOPTIC_API_KEY")){
+get_latest_data <- function(wh_station = "KDEN",
+                            wh_state = NULL,
+                            units = c("english", "metric"),
+                            token = Sys.getenv("SYNOPTIC_API_KEY")){
+
+  units <- match.arg(units)
 
   base_url <- "https://api.synopticdata.com/v2/"
 
   endpoint <- "stations/latest"
 
   if (is.null(wh_state)) {
-    req_url <- paste0(base_url, endpoint, "?token=", token, "&stid=", wh_station)
+    req_url <- paste0(base_url, endpoint, "?token=", token, "&stid=", wh_station, "&units=", units)
   } else {
     if (!(wh_state %in% datasets::state.abb)) { # check if state is valid
       stop("state not valid")
     }
-    req_url <- paste0(base_url, endpoint, "?token=", token, "&state=", wh_state)
+    req_url <- paste0(base_url, endpoint, "?token=", token, "&state=", wh_state, "&units=", units)
   }
 
   # Send the API request and get response
